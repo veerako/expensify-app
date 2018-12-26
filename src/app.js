bbfebfadb95746e9a7d564812eea08e5ad6523e2
avinash kvk <kvk@avinashs-MacBook-Pro-2.local>
2018-12-26 15:12:03 -0500

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -16,25 +16,6 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-store.dispatch(addExpense({description: 'Water bill', amount: 5000}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({description: 'rent', amount: 105000}));
-/*store.dispatch(setTextFilter('water'));
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-
-}, 3000)*/
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
-
-//addExpense
-//setTextFilter
-//getVisibleExpenses
-
- 
-
 const jsx = (
     <Provider store = {store}>
     <AppRouter/>
@@ -42,5 +23,25 @@ const jsx = (
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
+
+/*store.dispatch(addExpense({description: 'Water bill', amount: 5000}));
+store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000 }));
+store.dispatch(addExpense({description: 'rent', amount: 105000}));
+store.dispatch(setTextFilter('water'));
+setTimeout(() => {
+    store.dispatch(setTextFilter('bill'));
+
+}, 3000)
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
+console.log(visibleExpenses);*/
+
+//addExpense
+//setTextFilter
+//getVisibleExpenses
+
+ 
+
 
 
